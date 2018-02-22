929c2e359f9f757015ef44200fa8a5f7b283a7e4
Alex McKean <amckean@me.com>
2018-02-22 20:28:24 +0000

    Initial Commit

---
+++ NHLStats/config/environment.rb
@@ -0,0 +1,9 @@
+#Gems
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+#Models
+require_relative '../lib/cli.rb'
+require_relative '../lib/NHLStats.rb'
+require_relative '../lib/scraper.rb'
---
+++ NHLStats/lib/NHLStats.rb
@@ -0,0 +1,5 @@
+require "NHLStats/version"
+
+module NHLStats
+  # Your code goes here...
+end
